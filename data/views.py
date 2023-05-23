e4777a796d808d684f9a38764e44b43e47840bfe
VivekPatil81400 <59966933+VivekPatil81400@users.noreply.github.com>
2023-05-23 08:25:21 +0200

    started views.py

--- data/views.py
+++ data/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
-from docx import Document
+from .serializers import WordDataSerializer
+class WordDataAPIView(generics.GenericAPIView):
+    serializer_class = WordDataSerializer
 
+    def post(self, request):
+        word_file = request.FILES.get('C:\Users\vivek\Desktop\task\tkt_mock_data (1)')
 
+       
+
+        # Serialize the processed data using the serializer
+        serializer = self.get_serializer(data=processed_data)
+        serializer.is_valid(raise_exception=True)
+        serialized_data = serializer.data
+
+        return Response(serialized_data)
 
